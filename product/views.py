79fc0a1d8677b828296587ca42facdf3dfbff480
Boitumelo Rampete <u17120323@tuks.co.za>
2021-03-18 16:42:34 +0200

    MY_SQL

--- product/views.py
+++ product/views.py
@@ -47,7 +47,7 @@ class ProductDetail(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
+    def delete(self, request, pk, format=None): #delete product
         product = self.get_object(pk)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
